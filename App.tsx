db0b0a87bafc18945736f77e3651bbbf45b48852
Ani Jekova <angelina.jekova@rubberduck.xyz>
2020-11-20 17:24:09 +0200

    App.js clean and changed to tsx

---
+++ App.tsx
@@ -0,0 +1,19 @@
+/**
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  StatusBar,
+} from 'react-native';
+
+const App: () => React.ReactNode = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+    </>
+  );
+};
+
+export default App;
