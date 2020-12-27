b4dcc76430df08b88afe5cd4ac7b6f86d8ce17be
Ani Jekova <angelina.jekova@rubberduck.xyz>
2020-12-27 17:24:22 +0200

    async storage fixed and config file for storage keys added

--- App.tsx
+++ App.tsx
@@ -1,22 +1,23 @@
 import 'react-native-gesture-handler';
 import React, { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Alert, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import { CardContext } from './context/CartContext';
+
 import ShopNavigation from './components/ShopNavigation';
+
 import { ProductInterface } from './interfaces/product-interface';
 
-const STORAGE_KEY = '@storage_Card'; // TODO config
+import { STORAGE_KEY_CART } from './config/storage-keys';
 
 const App: () => React.ReactNode = () => {
   const [isStorageDataRead, setIsStorageDataRead] = useState<boolean>(false);
   const [cart, setCart] = useState<ProductInterface[]>([]);
 
-  console.log(cart);
   const storeData = async (array: ProductInterface[]) => {
     try {
-      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(array));
+      await AsyncStorage.setItem(STORAGE_KEY_CART, JSON.stringify(array));
     } catch (error) {
       console.log(error);
     }
@@ -24,11 +25,10 @@ const App: () => React.ReactNode = () => {
 
   const readData = async () => {
     try {
-      const storageArray = await AsyncStorage.getItem(STORAGE_KEY);
+      const storageArray = await AsyncStorage.getItem(STORAGE_KEY_CART);
 
       if (storageArray) {
         setCart(JSON.parse(storageArray));
-        setIsStorageDataRead(true);
       }
     } catch (error) {
       console.log(error);
@@ -38,6 +38,7 @@ const App: () => React.ReactNode = () => {
   useEffect(() => {
     if (!isStorageDataRead) {
       readData();
+      setIsStorageDataRead(true);
     }
   });
 
@@ -45,7 +46,7 @@ const App: () => React.ReactNode = () => {
     if (isStorageDataRead) {
       storeData(cart);
     }
-  }, [cart]);
+  }, [cart, isStorageDataRead]);
 
   return (
     <>
