df301cec35385db5bd3f90f550f46bab15594717
Ani Jekova <angelina.jekova@rubberduck.xyz>
2020-12-27 15:41:56 +0200

    async storage added for cart items

--- App.tsx
+++ App.tsx
@@ -1,20 +1,51 @@
 import 'react-native-gesture-handler';
-
-import React from 'react';
-import { StatusBar } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Alert, StatusBar } from 'react-native';
 
 import { CardContext } from './context/CartContext';
-
 import ShopNavigation from './components/ShopNavigation';
-
 import { Product } from './interfaces/product-interface';
 
-// TODO save cart to device storage
+const STORAGE_KEY = '@storage_Card'; // TODO config
 
 const App: () => React.ReactNode = () => {
-  const [cart, setCart] = React.useState<Product[]>([
-    { price: 10, name: 'tshirt' },
-  ]);
+  const [isStorageDataRead, setIsStorageDataRead] = useState<boolean>(false)
+  const [cart, setCart] = useState<Product[]>([]);
+
+  console.log(cart);
+  const storeData = async (array: Product[]) => {
+    try {
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(array));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const readData = async () => {
+    try {
+      const storageArray = await AsyncStorage.getItem(STORAGE_KEY);
+
+      if (storageArray) {
+        setCart(JSON.parse(storageArray));
+        setIsStorageDataRead(true);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!isStorageDataRead) {
+      readData();
+    }
+  });
+
+  useEffect(() => {
+    if (isStorageDataRead) {
+      storeData(cart);
+    }
+  }, [cart]);
 
   return (
     <>
